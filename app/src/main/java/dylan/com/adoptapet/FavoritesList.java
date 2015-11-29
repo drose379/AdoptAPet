accfa2ea389eb3df3fda673d2b9b4daef587bb22
Dylan Rose <dylanrose60@gmail.com>
2015-11-29 12:52:30 -0500

    Can now sort through animal types in favorties activity

--- app/src/main/java/dylan/com/adoptapet/FavoritesList.java
+++ app/src/main/java/dylan/com/adoptapet/FavoritesList.java
@@ -4,19 +4,15 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -25,8 +21,6 @@ import org.json.JSONObject;
 
 import java.util.ArrayList;
 
-import dylan.com.adoptapet.R;
-
 /**
  * Created by Dylan Rose on 11/22/15.
  */
@@ -34,6 +28,8 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
 
     private ProgressBar loader;
     private ListView favoritesList;
+
+    private TextView noFavorites;
 
     @Override
     public void onCreate(Bundle savedInstance) {
@@ -44,8 +40,9 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
         TextView toolbarTitle = (TextView) toolbar.findViewById(R.id.toolbarTitle);
         ImageView backButton = (ImageView) toolbar.findViewById(R.id.toolbarBackButton);
 
-        loader = (ProgressBar) findViewById( R.id.loader );
-        favoritesList = (ListView) findViewById( R.id.favoritesList );
+        loader = (ProgressBar) findViewById(R.id.loader);
+        favoritesList = (ListView) findViewById(R.id.favoritesList);
+        noFavorites = (TextView) findViewById(R.id.noFavorites);
 
 
         toolbarTitle.setText("My Favorites");
@@ -61,8 +58,6 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
     }
 
 
-
-
     public void loadFavorites() {
 
         final ArrayList<PetResult> favorites = new ArrayList<>();
@@ -71,39 +66,39 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
          * Check if lastUpdated is more then a day, if yes, send to the API to get updated, when they come back, update the individual items in the ArrayList, notifyDataUpdate
          */
 
-        SQLiteDatabase readable = new FavoritesDBHelper( this ).getReadableDatabase();
-        Cursor result = readable.rawQuery( "SELECT * FROM " + FavoritesDBHelper.table_name, null, null );
+        SQLiteDatabase readable = new FavoritesDBHelper(this).getReadableDatabase();
+        Cursor result = readable.rawQuery("SELECT * FROM " + FavoritesDBHelper.table_name, null, null);
 
-        if ( result.getCount() > 0 ) {
+        if (result.getCount() > 0) {
             /**
              * Get results, add each to favorites ArrayList
              */
 
             result.moveToPosition(-1);
 
-            while( result.moveToNext() ) {
+            while (result.moveToNext()) {
 
                 /**
                  * BUG: Not grabbing first value in Cursor
                  */
 
-                String id = result.getString( result.getColumnIndex( FavoritesDBHelper.id_col ) );
-                String type = result.getString( result.getColumnIndex( FavoritesDBHelper.type_col ) );
-                String name = result.getString( result.getColumnIndex( FavoritesDBHelper.name_col ) );
-                String age = result.getString( result.getColumnIndex( FavoritesDBHelper.age_col ) );
-                String sex = result.getString( result.getColumnIndex( FavoritesDBHelper.sex_col ) );
-                String size = result.getString( result.getColumnIndex( FavoritesDBHelper.size_col ) );
-                String breedString = result.getString( result.getColumnIndex( FavoritesDBHelper.breed_col ) );
-                String description = result.getString( result.getColumnIndex( FavoritesDBHelper.description_col ) );
-                String photo = result.getString( result.getColumnIndex( FavoritesDBHelper.photo_col ) );
+                String id = result.getString(result.getColumnIndex(FavoritesDBHelper.id_col));
+                String type = result.getString(result.getColumnIndex(FavoritesDBHelper.type_col));
+                String name = result.getString(result.getColumnIndex(FavoritesDBHelper.name_col));
+                String age = result.getString(result.getColumnIndex(FavoritesDBHelper.age_col));
+                String sex = result.getString(result.getColumnIndex(FavoritesDBHelper.sex_col));
+                String size = result.getString(result.getColumnIndex(FavoritesDBHelper.size_col));
+                String breedString = result.getString(result.getColumnIndex(FavoritesDBHelper.breed_col));
+                String description = result.getString(result.getColumnIndex(FavoritesDBHelper.description_col));
+                String photo = result.getString(result.getColumnIndex(FavoritesDBHelper.photo_col));
                 JSONObject contactInfo;
                 JSONArray photos;
                 try {
-                    contactInfo = new JSONObject( result.getString( result.getColumnIndex( FavoritesDBHelper.contactInfo_col ) ) );
-                    photos = new JSONArray( );
-                    JSONArray breeds = new JSONArray( breedString );
+                    contactInfo = new JSONObject(result.getString(result.getColumnIndex(FavoritesDBHelper.contactInfo_col)));
+                    photos = new JSONArray();
+                    JSONArray breeds = new JSONArray(breedString);
 
-                    photos.put( photo );
+                    photos.put(photo);
 
                     favorites.add(new PetResult()
                                     .setId(id)
@@ -118,65 +113,121 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
                                     .setPhotos(photos)
                     );
 
-                } catch ( JSONException e ) {
-                    throw new RuntimeException( e.getMessage() );
+                } catch (JSONException e) {
+                    throw new RuntimeException(e.getMessage());
                 }
 
             }
 
+            initAnimalTypeSpinner(favorites);
+
+            loader.setVisibility(View.GONE);
+            favoritesList.setVisibility(View.VISIBLE);
+            PetResultAdapter adapter = new PetResultAdapter(this, false, favorites);
+            favoritesList.setAdapter(adapter);
+
+            favoritesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView parent, View view, int which, long id) {
+                    Intent detail = new Intent(FavoritesList.this, PetResultDetail.class);
+                    detail.putExtra("pet", favorites.get(which));
+                    startActivity(detail);
+                }
+            });
 
         } else {
-            /**
-             * Show message that user has not favorited any animals, if they have a location saved, give button to make a search
-             */
+            loader.setVisibility(View.GONE);
+            favoritesList.setVisibility(View.GONE);
+            noFavorites.setVisibility(View.VISIBLE);
         }
 
+    }
 
-        loader.setVisibility( View.GONE );
-        favoritesList.setVisibility( View.VISIBLE );
-        PetResultAdapter adapter = new PetResultAdapter( this, false, favorites );
-        favoritesList.setAdapter( adapter );
 
-        favoritesList.setOnItemClickListener( new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick( AdapterView parent, View view, int which, long id ) {
-                Intent detail = new Intent( FavoritesList.this ,PetResultDetail.class );
-                detail.putExtra( "pet", favorites.get( which ) );
-                startActivity( detail );
+    private void initAnimalTypeSpinner(ArrayList<PetResult> favorites) {
+        /**
+         * Looks over types of animals in list, if more then 1, show spinner with types, if only 1, no need for spinner
+         */
+
+        final ArrayList<PetResult> favsPermanent = (ArrayList<PetResult>) favorites.clone();
+
+        Spinner typeSelect = (Spinner) findViewById(R.id.typeSelect);
+
+
+        ArrayList<String> types = new ArrayList<>();
+
+        types.add("Any");
+
+        for (PetResult item : favorites) {
+            if (!types.contains(item.getType())) {
+                types.add(item.getType());
             }
-        });
+        }
 
+        if (types.size() > 2) {
+
+            final String[] items = new String[types.size()];
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, types.toArray(items));
+            typeSelect.setAdapter(adapter);
+            typeSelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                @Override
+                public void onItemSelected(AdapterView parent, View v, int which, long id) {
+
+                    /**
+                     * Create temp array, pass to adapter, update the items in list
+                     */
+                    String type = items[which];
+                    PetResultAdapter listAdapter = (PetResultAdapter) favoritesList.getAdapter();
+
+
+                    ArrayList<PetResult> tempItems = new ArrayList<PetResult>();
+                    tempItems.clear();
+
+                    for (PetResult item : favsPermanent) {
+                        if (item.getType().equals(type)) {
+                            tempItems.add(item);
+                        }
+                        else if ( type.equals( "Any" ) ) {
+                            tempItems.add( item );
+                        }
+                    }
+                    
+                    listAdapter.updateAnimalType(tempItems);
+                    favoritesList.smoothScrollToPosition(0);
+
+                }
+
+                @Override
+                public void onNothingSelected (AdapterView parent){}
+
+            });
+
+            typeSelect.setVisibility(View.VISIBLE);
+
+        }
+
+        else
+
+        {
+            typeSelect.setVisibility(View.GONE);
+        }
 
     }
 
-
-
-    public String grabLocation() {
-        SQLiteDatabase readable = new ZipDBHelper( this ).getReadableDatabase();
-        Cursor result = readable.rawQuery( "SELECT zip FROM " + ZipDBHelper.table_name, null, null  );
-
-        result.moveToFirst();
-
-        String location = result.getString( result.getColumnIndex( "zip" ) );
-
-        readable.close();
-
-        return location;
-    }
 
     @Override
-    public void getResults( ArrayList<PetResult> results ) {
+    public void getResults(ArrayList<PetResult> results) {
         /**
          * Only used to update favorites that were last updated more then 1 day ago, check for each one
          */
     }
 
 
-
     @Override
-    public void onClick( View v ) {
-        switch ( v.getId() ) {
-            case R.id.toolbarBackButton :
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.toolbarBackButton:
 
                 finish();
 
@@ -184,8 +235,6 @@ public class FavoritesList extends AppCompatActivity implements View.OnClickList
 
         }
     }
-
-
 
 
 }
